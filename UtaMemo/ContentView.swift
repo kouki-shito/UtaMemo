9880ba4fa5e7afee06ee5447160db7aad0538209
kouki-shito <likegameskou@gmail.com>
2024-04-24 17:42:59 +0900

    Initial Commit

---
+++ UtaMemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UtaMemo
+//
+//  Created by 市東 on 2024/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UtaMemo/UtaMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtaMemoApp.swift
+//  UtaMemo
+//
+//  Created by 市東 on 2024/04/24.
+//
+
+import SwiftUI
+
+@main
+struct UtaMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
