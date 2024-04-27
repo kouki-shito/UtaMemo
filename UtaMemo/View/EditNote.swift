7e2986b65925d0ac6712dd0fa3ec4132ce213740
kouki-shito <likegameskou@gmail.com>
2024-04-27 20:22:30 +0900

    build iphone test

--- UtaMemo/View/EditNote.swift
+++ UtaMemo/View/EditNote.swift
@@ -15,6 +15,8 @@ struct EditNote: View {
   @Environment(\.dismiss) private var dismiss
   @State private var content : String = ""
   @State private var contentTitle : String = ""
+  @State private var CanUndo : Bool = false
+  @State private var CanRedo : Bool = false
   @FocusState var isFocussed : Bool
 
   var body: some View {
@@ -37,36 +39,69 @@ extension EditNote {
 
   private var navigationArea : some View {
     HStack(alignment:.center){
-      Button{
-        if isFocussed == false{
-          //Back Home
-          dismiss()
-        }else{
-          //disable Note Focus
-          isFocussed = false
-        }
-      }label: {
-        Image(
-          systemName:
-            isFocussed == false ? "chevron.backward":"checkmark.circle")
+      //MARK: -BACKANDFOCUS
+      HStack{
+        Button{
+          if isFocussed == false{
+            //Back Home
+            dismiss()
+          }else{
+            //disable Note Focus
+            isFocussed = false
+          }
+        }label: {
+          Image(
+            systemName:isFocussed == false ? "chevron.backward":"checkmark.circle")
           .font(.title)
           .fontWeight(.bold)
           .foregroundColor(
-            isFocussed == false ? .black : .mint)
+            isFocussed == false ? .black : .green)
+        }
+        .animation(.linear(duration: 0.1),
+                   value: isFocussed)
+        Spacer(minLength: 0)
       }
-      Spacer()
-      Circle()
-        .frame(width: 40,height: 40)
-      Circle()
-        .frame(width: 40,height: 40)
-      Button{
 
-      }label: {
-        Circle()
-        //.font(.title3)
-          .foregroundColor(.blue)
+      //MARK: -UNDOREDO
+        Button {
+          //Undo
+        }label: {
+          Image(systemName: "arrow.backward")
+            .font(.title2)
+            .opacity(isFocussed == true ? 1 : 0)
+
+        }
+        .padding(.leading)
+        .frame(width: 40,height: 40)
+        .disabled(!isFocussed || !CanUndo)
+        .animation(
+          .linear(duration: 0.1),
+           value:isFocussed)
+
+        Button{
+          //Redo
+        }label: {
+          Image(systemName: "arrow.forward")
+            .font(.title2)
+            .opacity(
+              isFocussed == true ? 1 : 0)
+        }
+        .frame(width: 40,height: 40,alignment: .center)
+        .disabled(!isFocussed || !CanRedo)
+        .animation(.linear(duration: 0.1), value:isFocussed)
+
+      //MARK: -MENUBUTTON
+      HStack{
+        Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+        Button{
+          //menuButton
+        }label: {
+          Image(systemName: "line.horizontal.3.circle")
+            .font(.title)
+        }
+        .frame(width: 40,height: 40)
+
       }
-      .frame(width: 40,height: 40)
     }
     .frame(height: 30)
     .padding(.horizontal)
@@ -91,7 +126,7 @@ extension EditNote {
       .fontWeight(.bold)
       .padding(.horizontal,20)
       .focused($isFocussed)
-      //.border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+    //.border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
       .scrollContentBackground(.hidden)//IOS 16~
     //.background(.gray)
   }
